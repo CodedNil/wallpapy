467e38b6afc79a2920c3ed2ea5d3889f956557ef
Dan <codednil@gmail.com>
2024-08-21 16:42:00 +0100

    Receive base64 image directly

--- src/image.rs
+++ src/image.rs
@@ -3,47 +3,45 @@ use async_openai::{
     types::{CreateImageRequestArgs, Image, ImageModel, ImageSize, ResponseFormat},
     Client,
 };
+use base64::{prelude::BASE64_STANDARD, Engine};
 use chrono::Utc;
-use reqwest::get;
 use std::path::Path;
-use tokio::{fs, io::AsyncWriteExt};
+use tokio::fs;
 
 pub async fn generate(prompt: &str) -> Result<String> {
     let client = Client::new();
 
-    let request = CreateImageRequestArgs::default()
-        .prompt(prompt)
-        .n(1)
-        .model(ImageModel::DallE3)
-        .response_format(ResponseFormat::Url)
-        .size(ImageSize::S1792x1024)
-        .user("wallpapy")
-        .build()?;
+    let response = client
+        .images()
+        .create(
+            CreateImageRequestArgs::default()
+                .prompt(prompt)
+                .n(1)
+                .model(ImageModel::DallE3)
+                .response_format(ResponseFormat::B64Json)
+                .size(ImageSize::S1792x1024)
+                .user("wallpapy")
+                .build()?,
+        )
+        .await?;
 
-    let response = client.images().create(request).await?;
-
-    let url = response
+    let b64_json = response
         .data
         .first()
-        .and_then(|arc_image| {
-            if let Image::Url { url, .. } = &**arc_image {
-                Some(url.clone())
-            } else {
-                None
-            }
+        .and_then(|arc_image| match **arc_image {
+            Image::B64Json { ref b64_json, .. } => Some(b64_json),
+            Image::Url { .. } => None,
         })
-        .ok_or_else(|| anyhow!("No valid image URL found"))?;
+        .ok_or_else(|| anyhow!("No valid image data found"))?;
 
-    let response = get(&url).await?;
+    let img_data = BASE64_STANDARD.decode(&**b64_json)?;
+
     let dir = Path::new("wallpapers");
     fs::create_dir_all(dir).await?;
 
-    let datetime = Utc::now().format("%Y-%m-%d_%H-%M-%S").to_string();
-    let file_name = format!("{datetime}.jpg");
-    let file_path = dir.join(file_name.clone());
-    let mut file = fs::File::create(file_path).await?;
-    let content = response.bytes().await?;
-    file.write_all(&content).await?;
+    let file_name = format!("{}.jpg", Utc::now().format("%Y-%m-%d_%H-%M-%S"));
+    let file_path = dir.join(&file_name);
+    fs::write(&file_path, img_data).await?;
 
     Ok(file_name)
 }
